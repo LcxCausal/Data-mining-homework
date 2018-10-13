b407f0c564221094f84301fa21e0118a49d2a66e
LcxCausal <153895208@qq.com>
2018-10-13 23:43:51 +0800

    update test2_2.

--- test2/src/test2_2.py
+++ test2/src/test2_2.py
@@ -13,50 +13,59 @@ from scipy.interpolate import lagrange
 
 
 # 通过lagrange插值算法对数组进行插值.
-def ploy_interpolate_column(s, n, k):
+def ploy_interpolate_column(data_array, index, range_count):
     """
-    :param s: 需要进行插值的数组（一维）
-    :param n: 需要计算插值的位置
-    :param k: 取插值位置前后的参考数据个数
+    :param data_array: 需要进行插值的数组（一维）
+    :param index: 需要计算插值的位置
+    :param range_count: 取插值位置前后的参考数据个数
     :return: 需要插入的值
     """
-    array = list(range(n - k, n)) + list(range(n + 1, n + 1 + k))
+    array = list(range(index - range_count, index)) + list(range(index + 1, index + 1 + range_count))
     y = []
     for index in array:
-        y.append(s[index])
-    return lagrange(array, y)(n)
+        y.append(data_array[index])
+    return lagrange(array, y)(index)
 
 
 # 定义文件路径
-cater_sale_missing_path = '../data/catering_sale_missing.xls'
+cater_sale_all_path = '../data/catering_sale_all.xls'
 missing_data_processing_path = '../data/missing_data_processing.xls'
 
 # 读取cater_sale_missing.xls文件内容
-cater_sale_missing_data = xlrd.open_workbook(cater_sale_missing_path)
-cater_sale_missing_sheet = cater_sale_missing_data.sheets()[0]
-cater_sale_rows_count = cater_sale_missing_sheet.nrows
+cater_sale_all_data = xlrd.open_workbook(cater_sale_all_path)
+cater_sale_all_sheet = cater_sale_all_data.sheets()[0]
+cater_sale_rows_count = cater_sale_all_sheet.nrows
+cater_sale_columns_count = cater_sale_all_sheet.ncols
 
 # 读取cater_sale_missing.xls文件的列名
-first_column_name = cater_sale_missing_sheet.row_values(0)[0]
-second_column_name = cater_sale_missing_sheet.row_values(0)[1]
+cater_sale_column_names = []
+cater_sale_first_row_data = cater_sale_all_sheet.row_values(0)
+for i in range(cater_sale_columns_count):
+    cater_sale_column_names.append(cater_sale_first_row_data[i])
 
 # 读取cater_sale_missing中的数据
-first_column_data = []
-second_column_data = []
-for i in range(1, cater_sale_rows_count):
-    first_column_data.append(cater_sale_missing_sheet.row_values(i)[0])
-    second_column_data.append(cater_sale_missing_sheet.row_values(i)[1])
+cater_sale_column_data = []
+for i in range(cater_sale_columns_count):
+    column_data = []
+    for j in range(1, cater_sale_rows_count):
+        column_data.append(cater_sale_all_sheet.row_values(j)[i])
+    cater_sale_column_data.append(column_data)
 
 # 获取缺失数据的索引
 missing_data_indexes = []
-for i in range(second_column_data.__len__()):
-    if second_column_data[i] == '':
-        missing_data_indexes.append(i)
+for i in range(cater_sale_column_data.__len__()):
+    missing_column_data_indexes = []
+    for j in range(cater_sale_rows_count - 1):
+        if str(cater_sale_column_data[i][j]).strip() == '':
+            missing_column_data_indexes.append(j)
+    missing_data_indexes.append(missing_column_data_indexes)
 
 # 对缺失的数据通过lagrange插值算法进行插值
-k = 5
-for i in missing_data_indexes:
-    second_column_data[i] = round(ploy_interpolate_column(second_column_data, i, k), 1)
+k = 2
+for i in range(missing_data_indexes.__len__()):
+    missing_column_data_indexes = missing_data_indexes[i]
+    for j in missing_column_data_indexes:
+        cater_sale_column_data[i][j] = round(ploy_interpolate_column(cater_sale_column_data[i], j, k), 1)
 
 # 写入missing_data_processing.xls文件中
 if os.path.exists(missing_data_processing_path):
@@ -68,13 +77,19 @@ missing_data_processing_sheet = missing_data_processing_data.add_sheet('missing_
 missing_data_processing_style = xlwt.XFStyle()
 missing_data_processing_style.num_format_str = 'YYY/M/D'
 
-missing_data_processing_sheet.write(0, 0, label=first_column_name)
-missing_data_processing_sheet.write(0, 1, label=second_column_name)
+for i in range(cater_sale_column_names.__len__()):
+    missing_data_processing_sheet.write(0, i, label=cater_sale_column_names[i])
 
-for i in range(first_column_data.__len__()):
-    missing_data_processing_sheet.write(i + 1, 0, first_column_data[i], missing_data_processing_style)
-    missing_data_processing_sheet.write(i + 1, 1, second_column_data[i])
+for i in range(cater_sale_column_data.__len__()):
+    column_data = cater_sale_column_data[i]
+    for j in range(column_data.__len__()):
+        if i == 0:
+            missing_data_processing_sheet.write(j + 1, i, column_data[j], missing_data_processing_style)
+        else:
+            missing_data_processing_sheet.write(j + 1, i, column_data[j])
 
 missing_data_processing_data.save(missing_data_processing_path)
+
+
 
 print('Completed!')
